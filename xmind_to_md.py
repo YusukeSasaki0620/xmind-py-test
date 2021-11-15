705d0428b3960cca9451aaf61f651c5a697b0d57
Yusuke Sasaki <yusuke.sasaki@playground.live>
2021-11-15 21:14:46 +0900

    feat: xmind_to_md

---
+++ xmind_to_md.py
@@ -0,0 +1,31 @@
+import glob,os,codecs
+import xmind
+input_dir = "input_files"
+os.chdir(input_dir)
+input_file_names = glob.glob("*.xmind")
+os.chdir("../")
+output_dir = 'output_files'
+LF = "\n"
+TAB = "  "
+LIST = "- "
+
+def sub_topic_recursive_processing(output_file , sub_topic, index = 0):
+  output_file.write(TAB * index + LIST + sub_topic.getTitle() + LF)
+  sub_topics = sub_topic.getSubTopics() or []
+  for sub_topic in sub_topics:
+    sub_topic_recursive_processing(output_file, sub_topic, index+1)
+
+
+print(input_file_names)
+
+for file_name in input_file_names:
+  input_file_path = input_dir + '/' + file_name
+  output_file_path = output_dir + '/' + os.path.splitext(file_name)[0] + '.md'
+  with codecs.open(output_file_path, 'w', 'utf-8') as f:
+    workbook = xmind.load(input_file_path)
+    sheets = workbook.getSheets()
+    for sheet in sheets:
+      f.write('# ' + sheet.getTitle() + LF)
+      rt = sheet.getRootTopic()
+      sub_topic_recursive_processing(f, rt)
+
