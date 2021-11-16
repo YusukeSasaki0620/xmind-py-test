273f92c82980d7ea9914b9442a521876387874d4
Yusuke Sasaki <yusuke.sasaki@playground.live>
2021-11-16 12:24:58 +0900

    feat: 引数対応

--- xmind_to_md.py
+++ xmind_to_md.py
@@ -1,12 +1,9 @@
 #-*- coding: utf-8 -*-
 
-import glob,os,codecs
+import glob,os,codecs,sys
 import xmind
 
 input_dir = "input_files"
-os.chdir(input_dir)
-input_file_names = glob.glob("*.xmind")
-os.chdir("../")
 output_dir = 'output_files'
 LF = "\n"
 TAB = "  "
@@ -21,20 +18,32 @@ def sub_topic_recursive_processing(output_file , sub_topic, index = 0):
   for sub_topic in sub_topics:
     sub_topic_recursive_processing(output_file, sub_topic, index+1)
 
+if __name__ == '__main__':
+  args = sys.argv
+  for index, arg in enumerate(args):
+    if index == 1: input_dir = arg
+    elif index == 2: output_dir = arg
+    else: pass
 
-print(input_file_names)
+  print("input_dir:" + input_dir)
+  print("output_dir:" + output_dir)
+  os.chdir(input_dir)
+  input_file_names = glob.glob("*.xmind")
+  os.chdir("../")
 
-for file_name in input_file_names:
-  input_file_path = input_dir + '/' + file_name
-  output_file_path = output_dir + '/' + os.path.splitext(file_name)[0] + '.md'
-  with codecs.open(output_file_path, 'w', 'utf-8') as f:
-    workbook = xmind.load(input_file_path)
-    sheets = workbook.getSheets()
-    for sheet in sheets:
-      f.write(SHEET + sheet.getTitle() + LF)
-      rt = sheet.getRootTopic()
-      f.write(ROOTTOPIK + rt.getTitle() + LF)
-      sub_topics = rt.getSubTopics() or []
-      for sub_topic in sub_topics:
-        sub_topic_recursive_processing(f, sub_topic)
+  print(input_file_names)
+
+  for file_name in input_file_names:
+    input_file_path = input_dir + '/' + file_name
+    output_file_path = output_dir + '/' + os.path.splitext(file_name)[0] + '.md'
+    with codecs.open(output_file_path, 'w', 'utf-8') as f:
+      workbook = xmind.load(input_file_path)
+      sheets = workbook.getSheets()
+      for sheet in sheets:
+        f.write(SHEET + sheet.getTitle() + LF)
+        rt = sheet.getRootTopic()
+        f.write(ROOTTOPIK + rt.getTitle() + LF)
+        sub_topics = rt.getSubTopics() or []
+        for sub_topic in sub_topics:
+          sub_topic_recursive_processing(f, sub_topic)
 
