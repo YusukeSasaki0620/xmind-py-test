c41dbed2665fda706faa649499feb5c3d6b485b9
Yusuke Sasaki <yusuke.sasaki@playground.live>
2021-11-16 22:11:57 +0900

    feat: topic書き出し対応

--- md_to_xmind.py
+++ md_to_xmind.py
@@ -4,6 +4,10 @@ import glob,os,codecs,re,sys
 import xmind
 from xmind.core.workbook import WorkbookDocument
 from xmind.core.topic import TopicElement
+from xmind.core.mixin import TopicMixinElement
+
+# NOTE:default 1000, 規模がでかくなるとエラーになる
+sys.setrecursionlimit(200000)
 
 input_dir = "output_files"
 output_dir = 'tmp'
@@ -39,40 +43,56 @@ if __name__ == '__main__':
       curr_line = f.readline()
       pre_line = ''
       curr_sheet = None
-      curr_topic = None
+      # NOTE:戻るために自分の親たちを覚えておく
+      curr_topics = None
+      # NOTE:戻る量を計算するため直前のネスト量を覚えておく
       curr_topic_prefix = None
-      tmp_cnt = 0
       while curr_line:
         match = re.match(SHEET, curr_line) or re.match(ROOTTOPIK, curr_line) or re.match(LISTREG, curr_line)
         if match:
           prefix = match.group()
           str_data = re.sub('^' + prefix, '', curr_line).replace(LF,'').encode('utf8')
           if prefix == SHEET:
-            print('sheet:' + str_data)
             sheet = w.createSheet()
             sheet.setTitle(str_data)
             w.addSheet(sheet)
             curr_sheet = sheet
           elif prefix == ROOTTOPIK:
-            print('roottopic:' + str_data)
             rt = curr_sheet.getRootTopic()
             rt.setTitle(str_data)
-            curr_topic = rt
+            # NOTE: curr_topicsをrootで初期化
+            curr_topics = [rt]
             curr_topic_prefix = prefix
-            tmp_cnt = 0
           else:
-            # TODO: いい感じに階層戻るやつをかんがえる
-            print('topic:' + str_data)
             tp=TopicElement()
             tp.setTitle(str_data)
-            if tmp_cnt < 100:
-              curr_topic.addSubTopic(tp)
-              print("index:" + str(tp.getIndex()))
-              tmp_cnt = tp.getIndex()
-            # curr_topic = tp
-            # curr_topic_prefix = prefix
+
+            now_prefix_len = len(prefix)
+            curr_prefix_len = len(curr_topic_prefix)
+
+            # NOTE: currがrootなら無条件で子供に追加
+            if curr_topic_prefix == ROOTTOPIK:
+              curr_topics[-1].addSubTopic(tp)
+              curr_topics.append(tp)
+              curr_topic_prefix = prefix
+            else:
+              # NOTE: 子ノードの追加ケース
+              # 深さ優先探索でmdを作ってある前提なので、len(now) > len(curr) -> currの子供
+              if now_prefix_len > curr_prefix_len:
+                curr_topics[-1].addSubTopic(tp)
+                curr_topics.append(tp)
+                curr_topic_prefix = prefix
+              else:
+                # NOTE: len(now) <= len(curr) -> currのN個親の子供、差分からNを求める
+                sub_topic_num = ( curr_prefix_len - now_prefix_len ) / len(TAB) + 1
+                while sub_topic_num > 0:
+                  curr_topics.pop()
+                  sub_topic_num -= 1
+                curr_topics[-1].addSubTopic(tp)
+                curr_topics.append(tp)
+                curr_topic_prefix = prefix
         else:
-          # TODO: TOPIK内改行をどうするか、、、
+          # NOTE: topic内改行をエスケープしてmdに出力しているので基本でない
           print('other:' + curr_line)
         pre_line = curr_line
         curr_line = f.readline()
