c9559c063cc5e1f16472ef1a78139851ca896dad
Yusuke Sasaki <yusuke.sasaki@playground.live>
2021-11-16 11:44:41 +0900

    feat: add sub_topic

--- md_to_xmind.py
+++ md_to_xmind.py
@@ -3,6 +3,7 @@
 import glob,os,codecs,re
 import xmind
 from xmind.core.workbook import WorkbookDocument
+from xmind.core.topic import TopicElement
 
 input_dir = "output_files"
 os.chdir(input_dir)
@@ -12,13 +13,10 @@ output_dir = 'tmp'
 LF = "\n"
 TAB = "  "
 LIST = "- "
-
-def sub_topic_recursive_processing(output_file , sub_topic, index = 0):
-  output_file.write(TAB * index + LIST + sub_topic.getTitle() + LF)
-  sub_topics = sub_topic.getSubTopics() or []
-  for sub_topic in sub_topics:
-    sub_topic_recursive_processing(output_file, sub_topic, index+1)
-
+# NOTE: xmind書き込みの際にメソッドが別れているので区別できるように
+SHEET = "# "
+ROOTTOPIK = "## "
+LISTREG = "(  ){0,}- "
 
 print(input_file_names)
 
@@ -30,17 +28,37 @@ for file_name in input_file_names:
 
     curr_line = f.readline()
     pre_line = ''
+    curr_sheet = None
+    curr_topic = None
+    curr_topic_prefix = None
     while curr_line:
-      if re.match('# ', curr_line):
-        print('sheet:' + re.sub('^# ', '', curr_line))
-        sheet = w.createSheet()
-        sheet.setTitle(re.sub('^# ', '', curr_line).encode('utf8'))
-        rt = sheet.getRootTopic()
-        rt.setTitle('fugafuga')
-        w.addSheet(sheet)
-      elif re.match("(  ){0,}- ", curr_line):
-        print('topic:' + curr_line)
+      match = re.match(SHEET, curr_line) or re.match(ROOTTOPIK, curr_line) or re.match(LISTREG, curr_line)
+      if match:
+        prefix = match.group()
+        str_data = re.sub('^' + prefix, '', curr_line).replace(LF,'').encode('utf8')
+        if prefix == SHEET:
+          print('sheet:' + str_data)
+          sheet = w.createSheet()
+          sheet.setTitle(str_data)
+          w.addSheet(sheet)
+          curr_sheet = sheet
+        elif prefix == ROOTTOPIK:
+          print('roottopic:' + str_data)
+          rt = curr_sheet.getRootTopic()
+          rt.setTitle(str_data)
+          curr_topic = rt
+          curr_topic_prefix = prefix
+        else:
+          # TODO: いい感じに階層戻るやつをかんがえる
+          print('topic:' + str_data)
+          tp=TopicElement()
+          tp.setTitle(str_data)
+          curr_topic.addSubTopic(tp)
+          print("index:" + str(tp.getIndex()))
+          # curr_topic = tp
+          # curr_topic_prefix = prefix
       else:
+        # TODO: TOPIK内改行をどうするか、、、
         print('other:' + curr_line)
       pre_line = curr_line
       curr_line = f.readline()
